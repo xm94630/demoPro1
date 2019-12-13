282a4ffd85b88d8f78b2e79446ec7d4f1bd3991b
xuming <xuming@geekplus.com.cn>
2019-12-13 17:22:35 +0800

    初始

---
+++ JY.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="JY">
+    鲸鱼
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'JY',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+<style scoped>
+.JY{
+  color:red;
+}
+</style>
---
+++ JY.vue.js
@@ -0,0 +1,11 @@
+
+export default {
+	template: `
+<div class="JY">
+  鲸鱼
+</div>`,
+  name: 'JY',
+  props: {
+    msg: String
+  }
+}
