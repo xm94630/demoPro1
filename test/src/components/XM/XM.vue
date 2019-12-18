feb3dcdfcc6a50de7446ce5dea3705fcb3c0fe8c
xm94630 <xm94630@126.com>
2019-12-18 23:46:45 +0800

    还不支持 import 引入第三方、其他vue的正确编译

--- test/src/components/XM/XM.vue
+++ test/src/components/XM/XM.vue
@@ -3,17 +3,22 @@
     这是一个可爱的组件。配置为：{{myConfig}}
     <div>从服务器获得的数据源：{{aaa}}</div>
     <el-button type="primary">element组件也是支持的</el-button>
-     <el-input-number v-model="num" @change="handleChange" :min="1" :max="10" label="描述文字"></el-input-number>
+    <el-input-number v-model="num" @change="handleChange" :min="1" :max="10" label="描述文字"></el-input-number>
+    <JY></JY>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import JY from "./JY"
 export default {
   name: 'XuMing',
   props: {
     msg: String,
     myConfig:null,
+  },
+  components: {
+    JY,
   },
   data() {
     return {
--- test/src/components/XM/XM.vue@Compile.js
+++ test/src/components/XM/XM.vue@Compile.js
@@ -1,17 +1,22 @@
 
 import axios from "axios";
+import JY from "./JY"
 export default {
 	template: `
 <div class="constomWidget XM">
   这是一个可爱的组件。配置为：{{myConfig}}
   <div>从服务器获得的数据源：{{aaa}}</div>
   <el-button type="primary">element组件也是支持的</el-button>
-   <el-input-number v-model="num" @change="handleChange" :min="1" :max="10" label="描述文字"></el-input-number>
+  <el-input-number v-model="num" @change="handleChange" :min="1" :max="10" label="描述文字"></el-input-number>
+  <JY></JY>
 </div>`,
   name: 'XuMing',
   props: {
     msg: String,
     myConfig:null,
+  },
+  components: {
+    JY,
   },
   data() {
     return {
@@ -24,7 +29,7 @@ export default {
       console.log(value);
     }
   },
-  mounted(){
+  mount(){
     axios.get('http://localhost:3000/epimetheus/api_v1/diy/view/info?diyViewCode=123').then(res => {
       this.aaa = res;
     });
